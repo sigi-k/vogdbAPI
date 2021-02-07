479d02efa7877f5a9eca54ea3ce3f4134c0b23be
Sigi Koizar <sigii@live.at>
2021-02-07 21:34:27 +0100

    debug uvicorn

--- debug.py
+++ debug.py
@@ -1,12 +1,13 @@
 import os
-from vogdb import api
+import logging
 import uvicorn
 
-# set the variables:
-os.environ["MYSQL_HOST"] = "localhost"
-os.environ["MYSQL_USER"] = "vog"
-os.environ["MYSQL_PASSWORD"] = "password"
-os.environ["MYSQL_DATABASE"] = "vogdb"
+if __name__ == '__main__':
+    # set the variables:
+    os.environ["MYSQL_HOST"] = "localhost"
+    os.environ["MYSQL_USER"] = "vog"
+    os.environ["MYSQL_PASSWORD"] = "password"
+    os.environ["MYSQL_DATABASE"] = "vogdb"
 
 
-uvicorn.run(api, port=8000)
+uvicorn.run("vogdb:api", port=8000, reload=True, access_log=False, log_level=logging.WARN)
