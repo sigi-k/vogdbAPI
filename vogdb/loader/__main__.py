40770e10fb74d96f82438b3c95e0578cc2048179
Sigi Koizar <sigii@live.at>
2021-02-10 16:34:24 +0100

    changes fixed

--- vogdb/loader/__main__.py
+++ vogdb/loader/__main__.py
@@ -7,9 +7,6 @@ from . import load_frames, save_db_sql
 
 data_dir = sys.argv[1] if len(sys.argv) > 1 else os.environ.get("VOG_DATA")
 
-if "test" in sys.argv:
-    data_dir = os.environ.get("VOG_DATA_TEST")
-
 if not data_dir:
     print(f"usage: {sys.argv[0]} <data directory>")
     sys.exit(2)
@@ -19,9 +16,3 @@ if data_dir[:-1] != "/":
 
 vog, species, protein, member = load_frames(data_dir)
 
-if "test" in sys.argv:
-    os.environ["MYSQL_DATABASE"] = os.environ["MYSQL_DATABASE_TEST"]
-    os.environ["MYSQL_HOST"] = os.environ["MYSQL_HOST_TEST"]
-    save_db_sql(database_url(), vog, species, protein, member)
-else:
-    save_db_sql(database_url(), vog, species, protein, member)
